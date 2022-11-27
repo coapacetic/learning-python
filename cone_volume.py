1848710d25d10b67514a39648fa0a94cea3c40bb
coapacetic <kyle.d.coapman@gmail.com>
2022-11-26 23:25:39 -0500

    feat/cone_volume.py

---
+++ cone_volume.py
@@ -0,0 +1,11 @@
+# Get the initial radius and height of the cone
+radius = int(input())
+height = int(input())
+
+# Calculate the volume
+PI = 3.1415
+V = (1/3) * PI * r ** 2 * h
+
+# Print the answer
+print(V)
+
